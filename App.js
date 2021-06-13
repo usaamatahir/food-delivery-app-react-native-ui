88e49fea9c3e665b4e315ee566b7b9f23ae5fc87
Usama Tahir <usamatahir8330@gmail.com>
2021-06-13 18:37:04 +0500

    Basic Template & Tab Screen

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Home, OrderDelivery, Restaurants } from "./screens";
+import Tabs from "./navigation/Tabs";
 
+const Stack = createStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Home" component={Tabs} />
+        <Stack.Screen name="Restaurants" component={Restaurants} />
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/index.js
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import Restaurants from "./Restaurants";
+import OrderDelivery from "./OrderDelivery";
+
+export { Home, Restaurants, OrderDelivery };
